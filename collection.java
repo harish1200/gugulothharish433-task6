2be3039d531b9049698d68c8247dceef5020b529
harish1200 <60343266+harish1200@users.noreply.github.com>
2020-03-06 21:46:02 +0530

    Update collection.java

--- collection.java
+++ collection.java
@@ -1,57 +1,71 @@
-import java.util.*;
-abstract class Newyear
-{
-void menu()
-{
-System.out.println("MENU");
-System.out.println("Chocolates Menu:");
-System.out.println("1.whiite Chocolates \n2.milk Chocolates \n3.dark Chocolates \n4.no more Chocolates\n");}
-void menu2(){
-System.out.println("Sweets Menu:");
-System.out.println("1.kajuSweets \n2.BadamSweets \n3.Dryfruitsweets \n4.no more swweets");
-}
-///void sort(int t,int price,int n);
- //void sort(int name,int p);
-}
-class Choco extends Newyear
-{
-float sort()
-{   Scanner sc=new Scanner(System.in);
-    System.out.println("enter the type of Chocolate you have");
-    int c=sc.nextInt();
-    System.out.println("enter number of chocolate pieces:");
-    int n=sc.nextInt();
-    int[] weight= new int[]{20,30,45,50};
+
+package set.Collections;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Linked{
+   public static void main(String args[]){
+
+     LinkedList<String> list=new LinkedList<String>();
+
+     //Adding elements to the Linked list
+     list.add("jagadish");
+     list.add("satish");
+     list.add("arun");
+     list.add("srikanth");
+     list.add("sai");
+     list.add("ashok");
+     list.add("naveen");
+     list.add("charan");
+     list.add("pranay");
+     list.add("santhosh");
+     list.add("kalyan");
+     list.add(11,"devendar");
+ 
+     System.out.println(list);
     
-    return weight[c]*n;
-}
-    
-}
-class sweets extends Newyear
-{
-float sort(int name,int n)
-{
-float weight_s=0;
-int price[]={40,50,27,85};
-return price[name]*n;
-}
-}
-public class Main
-{
-public static void main(String[] args)
-{
-Scanner sc=new Scanner(System.in);
-Choco c=new Choco();
-sweets s=new sweets();
-c.menu();
-float c_wt=c.sort();
-System.out.println("enter the name of sweet using its number given in menu:");
-s.menu2();
-int name=sc.nextInt();
-System.out.println("enter number of sweets:");
-int p=sc.nextInt();
-float s_wt=s.sort(name,p);
-System.out.println("toatl weight of gift:"+ (c_wt+s_wt));
-System.out.println("total number of candies and sweets in gift:"+ (p));
-}
+     //using index print value
+     System.out.println("LinkedList Elements :");
+     int i = 0;
+	while (list.size() > i) {
+   	  System.out.println(list.get(i));
+   	  i++;
+         }
+	
+     
+  //Adding an element to the first position
+     list.addFirst("nikhil");
+     
+     //Adding an element using position
+    list.add(2, "jaju");
+     list.add(5, "vilash");
+   
+//removing an element
+     list.remove("santhosh"); 
+     list.remove("ashok");  
+     list.remove("pranay");  
+  
+     //adding
+     list.add(11,"ilesh");
+     list.add("jaswanth");
+     System.out.println(list);
+     
+     //adding
+     list.add(1,"sai charan");
+     list.add(5,"vikranth");
+     list.add(7,"rahul LE");
+     list.add(9,"Subhash");
+     System.out.println(list);
+     //removing
+     list.remove("charan"); 
+     list.remove("arun");  
+     list.remove("satish");  
+     
+     System.out.println(list);
+     Iterator<String> iterator=list.iterator();
+     while(iterator.hasNext()){
+       System.out.println(iterator.next());
+         }
+   } 
 }
